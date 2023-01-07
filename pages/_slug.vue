671e4e44717f5b30e677c68828b2214c13a9cffd
ori <ori_larusa@hotmail.com>
2023-01-07 18:06:05 -0300

    puse mi rutas

---
+++ pages/_slug.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>{{$route.params.slug}}</p>
+    </template>
+    
+    <script>
+    export default { // nos permite generar cualquier ruta en nuestro proyecto.
+      name: 'ArticlePage',
+    }
+    </script>
+    
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-export default {
+export default { // nos permite generar cualquier ruta en nuestro proyecto.
   name: 'IndexPage',
 }
 </script>
