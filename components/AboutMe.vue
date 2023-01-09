b22ecff36a8d9942ad31ae27c977dd1efd83b6d1
ori <ori_larusa@hotmail.com>
2023-01-09 12:52:48 -0300

    se termino de agregar los datos de como se veria el articleCard componente

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="about-me">
-        <h1>Mini blog</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet numquam veritatis reprehenderit assumenda quasi totam. Hic, possimus! Ut, soluta neque! Quod est illo repellat, magni aliquid magnam fuga odio adipisci!</p>
+    <div class="aboutme justify-center text-center px-4 py-16">
+        <h1 class="font-weight-black pb-2">Mini blog</h1>
+        <p class="text--secondary text-md-left py-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet numquam veritatis reprehenderit assumenda quasi totam. Hic, possimus! Ut, soluta neque! Quod est illo repellat, magni aliquid magnam fuga odio adipisci!</p>
 
     </div>
 </template>
-    
+
+<style>
+.aboutme{
+    background-color:gray;
+}
+</style>
     
--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -1,10 +1,59 @@
 <template>
     <div class="article-card">
-        <h1>Article Card</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet numquam veritatis reprehenderit assumenda quasi totam. Hic, possimus! Ut, soluta neque! Quod est illo repellat, magni aliquid magnam fuga odio adipisci!</p>
-
+        <h3>{{ title }}</h3>
+        <div class="author">
+            <p>Por {{ author }}</p>
+            <!-- <small>Fecha de publicación: {{ date.toLoacleDateString() }}</small> -->
+            <small>Fecha de publicación: {{ date }}</small>
+        </div>
+        <figure>
+            <img :src="cover" alt="cover" />
+        </figure>
+        <p>{{ description }}</p>
+        <!-- <div class="actions">
+            <a class="btn" :href="slug">Ir al post</a>
+        </div> -->
+        <v-btn
+            depressed
+            color="primary"
+            :href="slug"
+            >
+            Ir al post
+        </v-btn>
+       
     </div>
 </template>
+<script>
+export default {
+  name: 'ArticleCard',
+  props: {
+    slug: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    author: {
+      type: String,
+      default: '',
+    },
+    date: {
+      type: Date,
+      default: '',
+    },
+    cover: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  }
+}
+</script>
 <style>
 .article-card{
     border:red 0.1rem;
@@ -12,6 +61,7 @@
     padding-left: 2rem;
     padding-right: 2rem;  
     flex: 0 0 33%;
+    margin-top:4rem ;
 }
 </style>
     
