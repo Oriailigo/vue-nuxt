cd3344a33eb16b1d815858de508569ffb37813bf
ori <ori_larusa@hotmail.com>
2023-02-28 13:30:10 -0300

    fin

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -27,23 +27,44 @@ export default {
   components: {
     VueMarkdown,
   },
-  data() {
-    return {
-      post: {
-        slug: 'mi-primer-post',
-        title: 'Mi ultra primer post',
-        author: 'Diana Martínez',
-        updated: '8/06/2022',
-        description: 'Lorem ispum dolor sit amet',
-        cover: 'https://via.placeholder.com/1024x420',
-        content: '# Title\n\n## Second title\n\nLorem ipsum dolor sit amet',
-      },
-    }
+  asyncData({ params, $http }) {
+    const { slug } = params;
+    const article = $http.$get(`http://localhost:9999/.netlify/functions/article?slug=${slug}`);
+    // eslint-disable-next-line no-console
+    console.log(article);
+
+    return article;
   },
+  // data() {
+  //   return {
+  //     post: {
+  //       slug: 'mi-primer-post',
+  //       title: 'Mi ultra primer post',
+  //       author: 'Diana Martínez',
+  //       updated: '8/06/2022',
+  //       description: 'Lorem ispum dolor sit amet',
+  //       cover: 'https://via.placeholder.com/1024x420',
+  //       content: '# Title\n\n## Second title\n\nLorem ipsum dolor sit amet',
+  //     },
+  //   }
+  // },
+
   head() {
     return {
       title: this.post?.title,
       meta: [{ name: 'description', content: this.post?.description || '' }],
+    }
+  },
+  computed: {
+    post() {
+      return {
+        title: this.article?.title,
+        author: this.article['author-name'][0],
+        updated: new Date(this.article?.updated).toLocaleDateString(),
+        description: this.article?.description,
+        cover: this.article?.cover[0].thumbnails.full.url,
+        content: this.article?.content,
+      }
     }
   },
 }
